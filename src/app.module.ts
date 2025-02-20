c4a6e8daf8a65109d5a0860f4f3625fbd88ad77c
sasith duleepa <duleepajayathissa@gmail.com>
2025-02-20 09:23:47 +0530

    add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UserModule } from './users/user.module';
+import { AuthMiddleware } from './common/middleware/auth.middleware';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('users');
+  }
+}
